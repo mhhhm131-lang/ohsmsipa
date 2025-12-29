784f9d5b9732cec16fd53ef9e44d61254da9d15e
mhhhm131 <mhhhm131@gmail.com>
2025-12-29 02:00:44 -0800

    update apps.py

--- backend/ohsms/apps.py
+++ backend/ohsms/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+import os
 
 class OhsmsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ohsms'
+
+    def ready(self):
+        User = get_user_model()
+
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "Admin@123")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                password=password,
+                email=email
+            )
