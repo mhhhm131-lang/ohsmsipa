3c057e7628c54acd82eae1d165839d13e04ec510
mhhhm131 <mhhhm131@gmail.com>
2025-12-31 05:44:36 -0800

    fix LOGIN_URL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -143,4 +143,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+LOGIN_URL = '/login/'
 
